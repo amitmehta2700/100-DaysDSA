3e53dea796bc186901b6f3fa29e64d96cdb6f42d
amitmehta2700 <amitmehta.2700@gmail.com>
2026-05-05 13:41:20 +0530

    Add files via upload

---
+++ Day20-01.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+
+            if(sum == 0) {
+                count++;
+            }
+        }
+    }
+
+    printf("Count of zero sum subarrays: %d\n", count);
+
+    return 0;
+}
