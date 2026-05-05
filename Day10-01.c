367b72be5e6f634ad25c8bd2f4c33dd09d0acba5
amitmehta2700 <amitmehta.2700@gmail.com>
2026-05-05 13:29:32 +0530

    Add files via upload

---
+++ Day10-01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int i,n,a[100],j;
+    printf("Enter the number of elements: ");   
+    scanf("%d",&n);
+    printf("Enter the elements: ");
+    for(i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    if 
+       (a[i]== a[n-i-1]) {
+            printf("Palindrome");
+        }
+        else {
+            printf("Not a palindrome");
+        }
+        return 0;
+
+}
