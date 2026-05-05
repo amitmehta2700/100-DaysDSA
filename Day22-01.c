5001eed50a00a377caa91d390262844b043ad4c7
amitmehta2700 <amitmehta.2700@gmail.com>
2026-05-05 13:42:10 +0530

    Add files via upload

---
+++ Day22-01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1; head->next = second;
+    second->data = 2; second->next = third;
+    third->data = 3; third->next = fourth;
+    fourth->data = 4; fourth->next = NULL;
+
+    int result = countNodes(head);
+    printf("Number of nodes: %d\n", result);
+
+    return 0;
+}
